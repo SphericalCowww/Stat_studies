26831281d73bfa0dcacc1c253172b835c2bc2d0f
SphericalCowww <tinglin194@gmail.com>
2023-01-30 01:14:29 +0100

    - several correction + hatch for plots

--- hypoTestingDef.py
+++ hypoTestingDef.py
@@ -14,14 +14,14 @@ from scipy import optimize
 from scipy import stats
 from tqdm import tqdm
 
-##########################################################################################
+###########################################################################################
 def main():
     verbosity = 1
     rangeX = [-7, 22]
     plotRes = 1000
     muSigNull = [0.0, 1.8]
     muSigAlt  = [5.9, 2.5]
-    dataLoc = 4.5
+    dataLoc = 2.4
 
     qVal     = np.linspace(*rangeX, plotRes)
     baseline = qVal*0.0
@@ -31,9 +31,9 @@ def main():
     qAlpha = muSigNull[0]+2*muSigNull[1]    #2 sigma-significance
     qVal_CRsel = (qVal > qAlpha)            #CR for critical region
     qVal_PVsel = (qVal > dataLoc)           #PV for p-value
-    qVal_PSsel = (qVal > dataLoc)           #PS for power/sensitivity
+    qVal_PSsel = (qVal > qAlpha)           #PS for power/sensitivity
 #plots
-    fig = plt.figure(figsize=(12, 7))
+    fig = plt.figure(figsize=(14, 7))
     gs = gridspec.GridSpec(1, 1)
     ax0 = fig.add_subplot(gs[0])
     
@@ -43,35 +43,39 @@ def main():
     
     ax0.get_xaxis().set_ticklabels([])
     ax0.get_yaxis().set_ticklabels([])
-    ax0.set_xlabel("statistics q(x), where x is data", fontsize=18,\
-                   horizontalalignment='right', x=1.0)
+    ax0.set_xlabel("statistics q($\mu$), where $\mu$ is the fit parameters to data",\
+                   fontsize=18, horizontalalignment='right', x=1.0)
     ax0.set_ylabel("probability density", fontsize=18)
     ax0.set_xlim(*rangeX)
     ax0.set_ylim(bottom=0.0)    
 
     dataLine = ax0.axvline(x=dataLoc, ymin=0.0, ymax=1.0, color="black", linewidth=2,\
                            linestyle="--")
-    CRfill = ax0.fill_between(qVal[qVal_CRsel], baseline[qVal_CRsel],\
-                              gausNull[qVal_CRsel], color='blue', alpha=0.5)
-    PVFill = ax0.fill_between(qVal[qVal_PVsel], baseline[qVal_PVsel],\
-                              gausNull[qVal_PVsel], color='blue', zorder=5)
-    PSfill = ax0.fill_between(qVal[qVal_PSsel], baseline[qVal_PSsel],\
-                              gausAlt[qVal_PSsel], color='red', alpha=0.5) 
-    plotList = [[nullDist, altDist, dataLine, CRfill, PVFill, PSfill], \
-                ["P(q|$\mu$=$\mu_s$) for signal null hypothesis",\
-                 "P(q|$\mu$=$\mu_0$) for no-signal alternative hypothesis", 
-                 "q(x$_{obs}$),\nx$_{obs}$: observed data",
+    CRfill = ax0.fill_between(qVal[qVal_CRsel], baseline[qVal_CRsel], gausNull[qVal_CRsel],\
+                              color="blue", alpha=0.5, zorder=0)
+    PVFill = ax0.fill_between(qVal[qVal_PVsel], baseline[qVal_PVsel], gausNull[qVal_PVsel],\
+                              color="none", hatch="///", edgecolor='blue')
+    PSfill = ax0.fill_between(qVal[qVal_PSsel], baseline[qVal_PSsel], gausAlt[qVal_PSsel],\
+                              color="none", alpha=0.5, hatch="...", edgecolor='red') 
+    blank = ax0.axvline(x=dataLoc, ymin=0.0, ymax=1.0, color="black", alpha=0.0)
+    plotList = [[nullDist, altDist, dataLine, CRfill, PVFill, PSfill, blank], \
+                ["P(q|$\mu$=$\mu_s$) with q for signal null hypothesis",\
+                 "P(q|$\mu$=$\mu_0$) with q for no-signal alternative hypothesis", 
+                 "q($\hat{\mu}_{data}$),\n"+\
+                 "$\hat{\mu}_{data}$: parameters $\mu$ as fit to observed data",
                  "P(q>q$_\\alpha$|$\mu_s$) $\equiv \\alpha$,\n"+\
                  "q$_\\alpha$: critical value, $\\alpha$: significance set to 0.05",\
-                 "P(q>q(x$_{obs}$)|$\mu_s$) $\equiv$ p$_\mu$,\np$_\mu$: p-value",\
-                 "P(q>q(x$_{obs}$)|$\mu_0$) $\equiv$ M_$\mu_0$($\mu_s$),\n"+\
-                 "M_$\mu_0$($\mu_s$): power/sensitivity"]]
-    legObj = ax0.legend(*plotList, loc="upper right", fontsize=14)
+                 "P(q>q($\hat{\mu}_{data}$)|$\mu_s$) $\equiv$ p$_\mu$,\np$_\mu$: p-value",\
+                 "P(q>q($\hat{\mu}_{data}$)|$\mu_0$) $\equiv$ M_$\mu_0$($\mu_s$),\n"+\
+                 "M_$\mu_0$($\mu_s$): power/sensitivity",\
+                 "\nP(q$_d$|$\mu$=$\mu_0$) $\equiv \\beta$ with q for "+\
+                 "no-signal null hypothesis,\nq$_d$: discovery potential"]]
+    legObj = ax0.legend(*plotList, loc="upper right", fontsize=12)
 
     ylim = ax0.get_ylim()
     ax0.text(qAlpha,  ylim[0]-0.03*(ylim[1]-ylim[0]), "q$_\\alpha$", fontsize=18,\
              ha='center')
-    ax0.text(dataLoc, ylim[1]-0.03*(ylim[1]-ylim[0]), "q(x$_{obs}$)", fontsize=18)
+    ax0.text(dataLoc, ylim[1]-0.04*(ylim[1]-ylim[0]), "q($\hat{\mu}_{data}$)", fontsize=18)
 #save plots
     exepath = os.path.dirname(os.path.abspath(__file__))
     filenameFig = exepath + "/hypoTestingDef.png"
@@ -79,7 +83,7 @@ def main():
     plt.savefig(filenameFig)
     if verbosity >= 1: print("Creating the following files:\n" + filenameFig)
 
-#########################################################################################
+#################################################@#########################################
 TOLERANCE = pow(10.0, -10)
 SNUMBER   = pow(10.0, -124)
 def gaussian(mu, sig, x):
@@ -88,7 +92,7 @@ def gaussian(mu, sig, x):
          *(1.0/(sig*np.sqrt(2.0*np.pi)))
     vals[vals < SNUMBER] = SNUMBER
     return vals
-#######################################################################################
+###########################################################################################
 if __name__ == "__main__":
     print("\n####################################################################Head")
     main()
